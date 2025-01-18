c89ecf5d07b6ad06de15d7355edfec64e15c8f25
arai1-godaddy <arai1@godaddy.com>
2025-01-19 02:45:25 +0530

    get and post comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::CommentsController < ApplicationController
+    def index
+        comments = Comment.where(post_id: params[:post_id])
+        render json: comments
+    end
+
+    def create
+        comment = Comment.new(comment_params)
+        if comment.save
+            render json: comment
+        else
+            render json: {error: 'Error creating comment'}
+        end
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content, :user_id, :post_id)
+    end
 end
